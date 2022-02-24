183b9a855a233d96cb307153abe1e2e8a8e3a1f7
RegulusZJY <92102569+RegulusZJY@users.noreply.github.com>
2022-02-24 09:49:48 +0800

    Add files via upload

---
+++ classwork1.py
@@ -0,0 +1,15 @@
+Name = input()
+print("My name is: " + Name)
+print(type(Name))
+
+Age = input()
+print("I am " + str(Age) + " years")
+print(type(Age))
+
+Address = input()
+print("I live in "+ Address)
+print(type(Address))
+
+Favorite_Sports = input()
+print("I like to play "+ Favorite_Sports)
+print(type(Favorite_Sports))
